4d5b46d4e9a7c2f72d86d3a8af8b0fb899679618
Mark Anthony Del Castillo <markjrmorale2@gmail.com>
2026-08-23 10:10:42 +0800

    added jinja template and csrf token

--- cmip/www/warehouse.py
+++ cmip/www/warehouse.py
@@ -1,5 +1,16 @@
 import frappe
 
 def get_context(context):
+    user = frappe.session.user
+    user_full_name = (
+        frappe.get_cached_value("User", user, "full_name")
+        or user
+        or "Guest"
+    )
+
+    context.csrf_token = frappe.sessions.get_csrf_token()
+    context.user_full_name = user_full_name
+    context.user_initial = user_full_name.strip()[:1].upper() or "G"
+    context.full_width = True
     context.no_cache = 1
-    frappe.local.flags.web_page = True
+    return context
